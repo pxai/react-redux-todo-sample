2e9a020c944e51505dbaed8168d56343392da634
pxai <pello_altadill@cuatrovientos.org>
2017-04-30 19:16:52 +0200

    ok, added actions, dispatch and action switch

--- components/redux-example.js
+++ components/redux-example.js
@@ -12,11 +12,37 @@ console.log('Starting redux ')
 // takes two arguments.
 // 1. previous state
 // 2. the action
+
+
+
 var reducer = (state = {name: 'Anonymous'} , action) => {
     //state = state || {name: 'Anonymous'};  // ES5: With this creates an default arg. ES6 default value
+    console.log('new action: ', action);
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {
+                    ...state,
+                    name: action.name
+                };
+        default:
+            return state;
+    }
     return state;
 }
 var store = redux.createStore(reducer);
 
 var currentState = store.getState(); // It gets the state
 console.log('current state', currentState);
+
+// ACTIONS
+// In this case we will dispatchActions
+// We will define actions, objects with a TYPE property
+var action = {
+    // Rule of thumb: upper case and underscores
+    type: 'CHANGE_NAME',
+    name: 'Eugene'  // we passs a namein this action
+};
+// We dispatch to the store,. Argument: our action
+store.dispatch(action);
+console.log('current state', store.getState());
+
--- components/redux-todo-example.js
+++ components/redux-todo-example.js
@@ -5,22 +5,25 @@ var redux = require('redux');
 console.log('Starting redux store for ToDo app')
 
 // THE STORE
-// the createStore argument needs to be a PURE FUNCTION
-//  var store = Redux.createStore();
-// in this case a reducer: takes your existing state and an action
-// and calculates a NEW STATE. It needs to have a default state {name: 'Anonymous'}
-// takes two arguments.
-// 1. previous state
-// 2. the action
+
 var stateDefault = {
     searchText: '',
     showCompleted: false,
     todos: []
 };
-// In this case we just return the default state
+
 var reducer = (state = stateDefault , action) => {
-    //state = state || {name: 'Anonymous'};  // ES5: With this creates an default arg. ES6 default value
-    return state;
+    //state = state || stateDefault;  // ES5: With this creates an default arg. ES6 default value
+    console.log('Incoming action: ', action);
+    switch (action) {
+        case 'CHANGE_SEARCH':
+            return {
+                ...state,
+                searchText: action.searchText
+            };
+        default:
+            return state;
+    }
 }
 
 var store = redux.createStore(reducer);
@@ -29,3 +32,12 @@ var currentState = store.getState(); // It gets the state
 console.log('current state for ToDO', currentState);
 // Equivalent
 console.log('current state for ToDO', store.getState());
+
+var action = {
+    // Rule of thumb: upper case and underscores
+    type: 'CHANGE_SEARCH',
+    name: 'Default search...'  // we passs a namein this action
+};
+// We dispatch to the store,. Argument: our action
+store.dispatch(action);
+console.log('current state for ToDo', store.getState());
